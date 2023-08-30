a570f5dd8587bd34fa6bb1bc2259e52725b066d2
Ozan Yetkin <ozan.yetkin.579@gmail.com>
2023-08-30 15:09:51 +0300

    app initialized and tested with expo go, intro wrapped up

---
+++ 01_intro.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const App = () => (
+    <Text style={{ margin: 64 }}>
+        Hello World!
+    </Text>
+);
+
+export default App;
